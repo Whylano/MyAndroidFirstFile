8142b0b72139bb7f80ad78f3b14ea2271ddf8ad1
Whylano <112239843+Whylano@users.noreply.github.com>
2023-01-28 03:48:17 +0900

    Feat : BTS1Picture Click First Motion Every Pic

--- app/src/main/java/com/reo/myandroidfirstfile/MainActivity.kt
+++ app/src/main/java/com/reo/myandroidfirstfile/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.reo.myandroidfirstfile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -15,8 +16,46 @@ class MainActivity : AppCompatActivity() {
         val image1=findViewById<ImageView>(R.id.btsImage1);
         image1.setOnClickListener {
             Toast.makeText(this,"1번 클릭 완료",Toast.LENGTH_LONG).show()
+            // 2. 화면이 클릭되면, 다음 화면으로 넘어가서, 사진을 크게보여줌
+            // 이때 com.reo.myandroidfirstfile패키지에 새로운 AndroidActivity를 생성해주고
+            // 해당하는 이미지를 크게 만들어 주는 activity_bts1.xml layout을 설정해준것을 실행되도록 만드는
+            // 로직구문 입니다
+            var intent = Intent(this, Bts1Activity::class.java)
+            startActivity(intent)
+
         }
-        // 2. 화면이 클릭되면, 다음 화면으로 넘어가서, 사진을 크게보여줌
+        //7개 image들의 이름을 다 붙이고, 각각 클릭 시, 다른 액티비티로 이동!
+        var image2 = findViewById<ImageView>(R.id.btsImage2)
+        var image3 = findViewById<ImageView>(R.id.btsImage3)
+        var image4 = findViewById<ImageView>(R.id.btsImage4)
+        var image5 = findViewById<ImageView>(R.id.btsImage5)
+        var image6 = findViewById<ImageView>(R.id.btsImage6)
+        var image7 = findViewById<ImageView>(R.id.btsImage7)
+
+        image2.setOnClickListener{
+            var intent = Intent(this, Bts2Activity::class.java)
+            startActivity(intent)
+        }
+        image3.setOnClickListener{
+            var intent = Intent(this, Bts3Activity::class.java)
+            startActivity(intent)
+        }
+        image4.setOnClickListener{
+            var intent = Intent(this, Bts4Activity::class.java)
+            startActivity(intent)
+        }
+        image5.setOnClickListener{
+            var intent = Intent(this, Bts5Activity::class.java)
+            startActivity(intent)
+        }
+        image6.setOnClickListener{
+            var intent = Intent(this, Bts6Activity::class.java)
+            startActivity(intent)
+        }
+        image7.setOnClickListener{
+            var intent = Intent(this, Bts7Activity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
